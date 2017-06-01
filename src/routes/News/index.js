7eab55063bf4bc1b3fb6b83e81abd922d4693864
xjq熊剑桥 <jqxiong@Ctrip.com>
2017-06-01 21:46:48 +0800

    update

--- src/routes/News/index.js
+++ src/routes/News/index.js
@@ -1,41 +1,61 @@
 import React, { Component } from 'react';
 import {
-	Animated,
-	View,
-	Text,
-	Easing
-} from 'react-native';
+  Animated,
+  View,
+  Text,
+  Image,
+  Easing,
+  StyleSheet
+}
+from 'react-native';
 
 import utils from '../../utils';
 
 export default class News extends Component {
-   constructor(props) {
-     super(props);
-     this.state = {
-       fadeAnim: new Animated.Value(0), // init opacity 0
-     };
-   }
-   componentDidMount() {
-     Animated.timing(          // Uses easing functions
-       this.state.fadeAnim,    // The value to drive
-       {
-       		toValue: utils.width * .9,
-       		duration: 1000,
-       		easing: Easing.elastic(1)
-       },           // Configuration
-     ).start();      // Don't forget start!
-   }
-   render() {
-     return (
-       <View>
-       	<Animated.View
-       	  style={{
-       	  	width: this.state.fadeAnim,
-       	  	height: utils.halfPix,
-       	  	backgroundColor: '#d00'
-       	  }}>
-       	</Animated.View>
-       </View>
-     );
-   }
- }
+  constructor(props) {
+    super(props);
+    this.state = {
+      launchOpacity: new Animated.Value(1) // 透明度变化
+    }
+  }
+  componentDidMount() {
+    // 执行启动动画
+    this.runLaunchAnimation();
+  }
+  render() {
+    return (
+      <View style={styles.wrap}>
+        <Animated.View style={[styles.launch, {
+          opacity: this.state.launchOpacity
+        }]}>
+          <Image source = {require('../../img/clear.png')} style={styles.map} />
+        </Animated.View>
+       </View >
+    );
+  }
+  runLaunchAnimation() {
+    Animated.timing(
+      this.state.launchOpacity,
+      {
+        toValue: 0,
+        duration: 2000
+      }
+    ).start()
+  }
+}
+
+const styles = StyleSheet.create({
+  wrap: {
+    flex: 1
+  },
+  launch: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    backgroundColor: '#d00'
+  },
+  map: {
+    width: 50,
+    height: 50
+  }
+})
